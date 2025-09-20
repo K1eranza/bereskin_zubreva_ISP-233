30a7a8fdf47abdb5f4873ae65621ef3efc54fcda
vasy <zdan2007@gmail.com>
2025-09-20 13:17:30 +0300

    change

--- src/App.kt
+++ src/App.kt
@@ -1,18 +1,112 @@
 fun main() {
-    print("Введите первое число: ")
-    val number1 = readln().toInt()
-    print("Введите второе число: ")
-    val number2 = readln().toInt()
-    if (number1 > number2) {
-        println("Первое число больше")
-    } else if (number1 == number2) {
-        println("Числа равны")
-    } else {
-        println("Второе число больше")
-    }
-    val max = if (number1 > number2) number1 else number2
-    println("Максимальное число: $max")
-    val age = readln().toInt()
-    val status = if(age >= 18) "Взрослый" else "Ребенок"
-    println(status)
-}
+//    print("Введите первое число: ")
+//    val number1 = readln().toInt()
+//    print("Введите второе число: ")
+//    val number2 = readln().toInt()
+//    if (number1 > number2) {
+//        println("Первое число больше")
+//    } else if (number1 == number2) {
+//        println("Числа равны")
+//    } else {
+//        println("Второе число больше")
+//    }
+//    val max = if (number1 > number2) number1 else number2
+//    println("Максимальное число: $max")
+//    val age = readln().toInt()
+//    val status = if(age >= 18) "Взрослый" else "Ребенок"
+//    println(status)
+//    val numbers = 1..100
+//    val symbol 'a'..'z'
+
+
+//    println("Введите порядковый номер месяца (1-12): ")
+//    val index = readln().toInt()
+//    var month = ""
+//    if (index == 1) {
+//        month = "January"
+//    } else if (index == 2) {
+//        month = "February"
+//    }
+//    if (index == 3) {
+//        month = "March"
+//    } else if (index == 4) {
+//        month = "April"
+//    }
+//    if (index == 5) {
+//        month = "May"
+//    } else if (index == 6) {
+//        month = "June"
+//    }
+//    if (index == 7) {
+//        month = "July"
+//    } else if (index == 8) {
+//        month = "August"
+//    }
+//    if (index == 9) {
+//        month = "September"
+//    } else if (index == 10) {
+//        month = "October"
+//    }
+//    if (index == 11) {
+//        month = "November"
+//    } else if (index == 12) {
+//        month = "December"
+//    }
+//    println("Your month: $month")
+//    val index = readln().toInt()
+//    val month = when (index) {
+//        1 -> "Январь"
+//        2 -> "Февраль"
+//        3 -> "Март"
+//        4 -> "Апрель"
+//        5 -> "Май"
+//        6 -> "Июнь"
+//        7 -> "Июль"
+//        8 -> "Август"
+//        9 -> "Сентябрь"
+//        10 -> "Октябрь"
+//        11 -> "Ноябрь"
+//        12 -> "Декабрь"
+//        else -> "отсутствует месяц"
+//
+//    }
+//    if (index in 1..12) {
+//        println("Ваш индекс $index соответсвует месяцу $month")
+//    } else {
+//        println("Индекс вне диапазона")
+//    }
+
+// Мини - задание. Магический щит
+//    val damage = readln().toInt()
+//    val shield = 100 - damage
+//    println("Щит активен? (введите да или нет): ")
+//    val index = readln()
+//    val answer = when(index) {
+//        "да" -> ""
+//        "нет" -> ""
+//        else -> "введите да или нет"
+//    }
+//    if (index in "да") {
+//        println("Полученный урон: $shield")
+//    } else {
+//        println("Щит неактивен")
+//    }
+
+    //Задача "доступ в закрытую зону игры"
+//    print("Введите уровень игрока: ")
+//    val level = readln().toInt()
+//    println("Введите (true/false) если есть VIP: ")
+//    val hasVIP = readln().toBoolean()
+//    if ((level >= 30 && hasVIP) || level >= 50) {
+//        println("Доступ разрешен")
+//    } else {
+//        println("Доступ запрещен")
+//    }
+//}
+
+    //
+
+
+
+
+
